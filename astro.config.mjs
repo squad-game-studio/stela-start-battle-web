998e910a53142da4154bfd52af1568df9f3f8c9c
pochedev <pochedev8@gmail.com>
2024-10-10 17:27:53 -0400

    refactor: github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://squad-game-studio.github.io',
+    base: 'stela-start-battle-web',
+});
